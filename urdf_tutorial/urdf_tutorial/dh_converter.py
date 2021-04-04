3a2fb50d73a21983cbcda3586bd9f4e4f1266e23
ereszka <01149421@pw.edu.pl>
2021-04-04 18:59:54 +0200

    dh converter, doesnt work but converts files:)

--- urdf_tutorial/urdf_tutorial/dh_converter.py
+++ urdf_tutorial/urdf_tutorial/dh_converter.py
@@ -1,10 +1,51 @@
-class Converter:
+import json
+import yaml
 
-    def __init__(self, dh_matrix, name):
-        self.name = name
-        self.dh_matrix = dh_matrix
+'''
+zalozenia: 
+ruchome: arm1, arm2, wrist
+nieruchome: base, gripper
+'''
 
-    def convert():
-        pass
+def convert(jsonfile):
+
+    data = read_dh(jsonfile)
+  
+    # czy dodawac fixed, prismatic, continuous?
+
+    for item in data.values():
+        item['rpy'] = {}
+        item['xyz'] = {}
+        item['rpy']['r'] = 0
+        item['rpy']['p'] = item['alfa']
+        item['rpy']['y'] = 0
+
+        item['xyz']['x'] = item['a']
+        item['xyz']['y'] = 0
+        item['xyz']['z'] = item['d']
+
+        item.pop('alfa')
+        item.pop('theta')
+        item.pop('a')
+        item.pop('d')
+    
+    return data
+
+
+def read_dh(jsonfile):
+    with open(jsonfile, "r") as file:
+        data = json.load(file)    
+    return data
+
+def write_yaml(data, filename):
+    with open(filename, "w") as yamlfile:
+        yaml.dump(data, yamlfile)
+
+
+if __name__ == "__main__":
+    data = convert("dh.json")
+    write_yaml(data, "dh.yaml")
+
+
 
 
