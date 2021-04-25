dcc7df592d2da90f6827fc8363e5c15b4fc91cff
ereszka <01149421@pw.edu.pl>
2021-04-26 01:26:55 +0200

    zmiana taktyki

--- urdf_lab3/urdf_lab3/kdl_dkin.py
+++ urdf_lab3/urdf_lab3/kdl_dkin.py
@@ -40,15 +40,13 @@ class KdlDkin(Node):
         # get joint state
         [self.arm1connect, self.arm2connect, self.wristconnect] = msg.position
 
-        self.set_pose_stamped()
-
-        print(self.pose_stamped.pose.position.x)
-        print(self.pose_stamped.pose.position.y)
-        print(self.pose_stamped.pose.position.z)
+        print(self.pose_stamped.pose.position)
         print(self.pose_stamped.pose.orientation)
 
         # publish pose of gripper
         now = self.get_clock().now()
+        # self.set_pose_stamped()
+        self.get_pose_stamped()
         self.pose_stamped.header.stamp = now.to_msg()
         self.pose_stamped.header.frame_id = 'base'
         self.pose_pub.publish(self.pose_stamped)
@@ -56,22 +54,81 @@ class KdlDkin(Node):
 
     def set_pose_stamped(self):
         position, quaternion = self.solve_gripper_params()
-        
+
         self.pose_stamped.pose.position.x = float(position[0])
         self.pose_stamped.pose.position.y = float(position[1])
         self.pose_stamped.pose.position.z = float(position[2])
 
-        x,y,z,w = quaternion[0], quaternion[1], quaternion[2], quaternion[3]
-        self.pose_stamped.pose.orientation = Quaternion(x = float(x), y= float(y), z= float(z), w = float(w))
+        self.pose_stamped.pose.orientation.x = float(quaternion[0])
+        self.pose_stamped.pose.orientation.y = float(quaternion[1])
+        self.pose_stamped.pose.orientation.z = float(quaternion[2])
+        self.pose_stamped.pose.orientation.w = float(quaternion[3])
+
+
+    def get_pose_stamped(self):
+        chain = Chain()
+
+        rpy = self.read_yaml(os.path.dirname(os.path.realpath(__file__)) + '/dh.yaml')
+
+        r = rpy['arm1']['rpy']['r']
+        p = rpy['arm1']['rpy']['p']
+        y = rpy['arm1']['rpy']['y']
+        x = rpy['arm1']['xyz']['x']
+        y = rpy['arm1']['xyz']['y']
+        z = rpy['arm1']['xyz']['z']
+
+        arm1_frame = Frame(Rotation.RPY(r,p,y), Vector(x,y,z))
+        
+        r = rpy['arm2']['rpy']['r']
+        p = rpy['arm2']['rpy']['p']
+        y = rpy['arm2']['rpy']['y']
+        x = rpy['arm2']['xyz']['x']
+        y = rpy['arm2']['xyz']['y']
+        z = rpy['arm2']['xyz']['z']
+
+        arm2_frame = Frame(Rotation.RPY(r,p,y), Vector(x,y,z))
+
+        r = rpy['wrist']['rpy']['r']
+        p = rpy['wrist']['rpy']['p']
+        y = rpy['wrist']['rpy']['y']
+        x = rpy['wrist']['xyz']['x']
+        y = rpy['wrist']['xyz']['y']
+        z = rpy['wrist']['xyz']['z']
+
+        wrist_frame = Frame(Rotation.RPY(r,p,y), Vector(x,y,z))
+
+        chain.addSegment(Segment(Joint(Joint.RotZ), arm1_frame))
+        chain.addSegment(Segment(Joint(Joint.RotZ), arm2_frame))
+        chain.addSegment(Segment(Joint(Joint.TransZ), wrist_frame))
+
+        result = ChainFkSolverPos_recursive(chain)
+        gripper = Frame()
+
+        joint_array = JntArray(3)
+        joint_array[0] = self.arm1connect
+        joint_array[1] = self.arm2connect
+        joint_array[2] =self.wristconnect
+
+        result.JntToCart(joint_array, gripper)
+
+        quaternion = gripper.M.GetQuaternion()
+        position = gripper.p
+
+        self.pose_stamped.pose.position.x = float(position[0])
+        self.pose_stamped.pose.position.y = float(position[1])
+        self.pose_stamped.pose.position.z = float(position[2])
+
+        self.pose_stamped.pose.orientation.x = float(quaternion[0])
+        self.pose_stamped.pose.orientation.y = float(quaternion[1])
+        self.pose_stamped.pose.orientation.z = float(quaternion[2])
+        self.pose_stamped.pose.orientation.w = float(quaternion[3])
 
     def solve_gripper_params(self):
         # solving forward kinematics
         result = self.solve_kdl(self.get_chain())
 
-        # converting to quaternion
-        # gripper = self.get_frame("gripper")
         gripper = Frame()
-        cart = result.JntToCart(self.get_joint_array(), gripper)
+        result.JntToCart(self.get_joint_array(), gripper)
         quaternion = gripper.M.GetQuaternion()
         position = gripper.p
 
@@ -82,35 +139,23 @@ class KdlDkin(Node):
         return ChainFkSolverPos_recursive(chain)
 
     def get_joint_array(self):
-        # joint_array = JntArray(5)
         joint_array = JntArray(3)
-
-        # joint_array[0] = 0.0
-        # joint_array[1] = self.arm1connect
-        # joint_array[2] = self.arm2connect
-        # joint_array[3] = self.wristconnect
-        # joint_array[4] = 0.0 # nie mam pojecia czy to ma sens
 
         joint_array[0] = self.arm1connect
         joint_array[1] = self.arm2connect
-        joint_array[2] = self.wristconnect
+        joint_array[2] = -self.wristconnect
 
         return joint_array
 
     def get_chain(self):
         chain = Chain()
-
-        # base = Segment(Joint(), self.get_frame('base'))
         arm1 = Segment(Joint(Joint.RotZ), self.get_frame('arm1'))
         arm2 = Segment(Joint(Joint.RotZ), self.get_frame('arm2'))
         wrist = Segment(Joint(Joint.TransZ), self.get_frame('wrist'))
-        # gripper = Segment(Joint(), self.get_frame('gripper'))
 
-        # chain.addSegment(base)
         chain.addSegment(arm1)
         chain.addSegment(arm2)
         chain.addSegment(wrist)
-        # chain.addSegment(gripper)
     
         return chain
 
