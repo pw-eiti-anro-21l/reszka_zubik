892e2ca4045cab09b19e9e4cdf8a9fb6ab659b3b
lena <01149451@pw.edu.pl>
2021-05-07 13:29:57 +0200

    odom error

--- urdf_lab4/urdf_lab4/jint.py
+++ urdf_lab4/urdf_lab4/jint.py
@@ -1,13 +1,87 @@
-from math import sin, cos, atan2, sqrt
+import sys
+
+from Interpolation.srv import Interpolation
 import rclpy
 from rclpy.node import Node
-from rclpy.qos import QoSProfile
-from geometry_msgs.msg import Quaternion
-from sensor_msgs.msg import JointState
-from geometry_msgs.msg import PoseStamped
 
 
-class Jint(Node):
+class MinimalClientAsync(Node):
 
     def __init__(self):
-        super().__init__('jint')
+        super().__init__('minimal_client_async')
+        self.cli = self.create_client(Interpolation, 'interpolation')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = Interpolation.Request()
+
+    def send_request(self):
+        # #from tutorial srv cli
+        # self.req.a = int(sys.argv[1])
+        # self.req.b = int(sys.argv[2])
+        # self.future = self.cli.call_async(self.req)
+        try:
+            if(float(sys.argv[1])>0 or float(sys.argv[1]) < -1):
+                self.get_logger().info('Niepoprawna wartośc przesunięcia dla członu 1') 
+                raise ValueError()
+            else:
+                self.req.joint1_goal = float(sys.argv[1])
+
+            if(float(sys.argv[2])>0 or float(sys.argv[2]) < -3):
+                self.get_logger().info('Niepoprawna wartośc przesunięcia dla członu 2')                
+                raise ValueError()
+            else:
+                self.req.joint2_goal= float(sys.argv[2])
+
+            if(float(sys.argv[3])>0 or float(sys.argv[3]) < -2):
+                self.get_logger().info('Niepoprawna wartośc przesunięcia dla członu 3')
+                raise ValueError()
+            else:
+                self.req.joint3_goal = float(sys.argv[3])
+
+            if(float(sys.argv[4])<=0):
+                self.get_logger().info('Niepoprawna wartość czasu')
+                raise ValueError()
+            else:
+                self.req.time_of_move = float(sys.argv[4])
+
+            if(str(sys.argv[5]) !='linear' and str(sys.argv[5]) !='polynomial' ):
+                self.get_logger().info('Zły typ interpolacji ')
+                raise ValueError()
+            else:
+                self.req.type = (sys.argv[5])  
+        except IndexError:
+            print("Niepoprawna liczba parametrów")
+            raise Exception()
+        except ValueError:
+            print("Błędne parametry")
+            raise Exception()
+            
+        self.future = self.cli.call_async(self.req)
+    
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_client = MinimalClientAsync()
+    minimal_client.send_request()
+
+    while rclpy.ok():
+        rclpy.spin_once(minimal_client)
+        if minimal_client.future.done():
+            try:
+                response = minimal_client.future.result()
+            except Exception as e:
+                minimal_client.get_logger().info(
+                    'Service call failed %r' % (e,))
+            else:
+                minimal_client.get_logger().info(
+                    'Result of interpolation: joint1:%d, joint2:%d, joint3:%d, time:%d = %s' %
+                    (minimal_client.req.arm1_pos, minimal_client.req.arm2_pos, minimal_client.req.wrist_pos, minimal_client.req.time, response.output))
+            break
+
+    minimal_client.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
--- urdf_lab4/urdf_lab4/jint_control_srv.py
+++ urdf_lab4/urdf_lab4/jint_control_srv.py
@@ -1,36 +1,85 @@
+from Interpolation.srv import Interpolation
+
 import rclpy
 from rclpy.node import Node
-from rclpy.qos import QoSProfile
-
-from interpolation.srv import Interpolation
 
 
-class JintControlService(Node):
+class MinimalService(Node):
 
     def __init__(self):
-        super().__init__('jint_service')
-        self.service = self.create_service(Interpolation, 'interpolation', interpolation_callback)
+        super().__init__('minimal_service')
+        self.srv = self.create_service(Interpolation, 'interpolation', self.interpolation_callback)
+
+    def interpolation_callback(self, request, response):
+        # #tutorial srv cli
+        # response.sum = request.a + request.b
+        # self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
+
+        # Pozycje początkowe w stawach
+        start_positions = [0, 0, 0]
+                                               
+        self.get_logger().info('Incoming request')
 
 
-    def interpolation_callback(self, msg, output):
+        sample_time = 0.1 # przykładowo co 0.1s wysyłamy wiadomość
+        total_time = request.time_of_move
+        steps = math.floor(total_time/sample_time) # całkowita liczba kroków do pętli
 
-        time = msg.time
-        freq = 0.1
+        # Wyznaczenie współczynników dla metody wielomianowej
+        if(request.type == 'polynomial'):
+            a0 = [start_positions[0], start_positions[1], start_positions[2]]
+            a2 = [3*((request.joint1_goal - start_positions[0])/(request.time_of_move)**2),
+            3*((request.joint2_goal - start_positions[1])/(request.time_of_move)**2),
+            3*((request.joint3_goal - start_positions[2])/(request.time_of_move)**2)]
+            a3 = [-2*((request.joint1_goal - start_positions[0])/(request.time_of_move)**3),
+            -2*((request.joint2_goal - start_positions[1])/(request.time_of_move)**3),
+            -2*((request.joint3_goal - start_positions[2])/(request.time_of_move)**3),]
 
-        if msg.type is "linear":
-            pass
-        
-        else:
-            pass
-            # inne metody lub error ze niepoprawne dane
+        for i in range(1,steps+1):
+            
+            # Publisher dla Kartmana
+            qos_profile1 = QoSProfile(depth=10)
+            self.joint_pub = self.create_publisher(JointState, '/joint_states', qos_profile1)
+            joint_state = JointState()
+            now = self.get_clock().now()
+            joint_state.header.stamp = now.to_msg()
+            joint_state.name = ['arm1connect', 'arm2connect', 'wristconnect']
+
+            # Interpolacja liniowa
+            if(request.type == 'linear'):
+                joint1_next = start_positions[0] + ((request.joint1_goal - start_positions[0])/request.time_of_move)*sample_time*i
+                joint2_next = start_positions[1] + ((request.joint2_goal - start_positions[1])/request.time_of_move)*sample_time*i
+                joint3_next = start_positions[2] + ((request.joint3_goal - start_positions[2])/request.time_of_move)*sample_time*i
+
+            # interpolacja wielomianowa
+            if(request.type == 'polynomial'):
+                joint1_next = a0[0] + a2[0]*(sample_time*i)**2 + a3[0]*(sample_time*i)**3 
+                joint2_next = a0[1] + a2[1]*(sample_time*i)**2 + a3[1]*(sample_time*i)**3 
+                joint3_next = a0[2] + a2[2]*(sample_time*i)**2 + a3[2]*(sample_time*i)**3 
+
+            #Przypisanie wartości dla członów robota
+            joint_state.position = [float(joint1_next), float(joint2_next), float(joint3_next)]
+
+            # Publikowanie połozenia człónów robota
+            self.joint_pub.publish(joint_state)
+            time.sleep(sample_time)
+
+
+
+        response.confirmation = 'Interpolacja zakończona'
+
+        return response
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_service = MinimalService()
+
+    rclpy.spin(minimal_service)
+
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
-
-    rclpy.init(args=args)
-
-    jint_control_service = JintControlService()
-
-    rclpy.spin(jint_control_service)
-
-    rclpy.shutdown()
+    main()
